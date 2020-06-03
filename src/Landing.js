6980fc85cda7798515370a3bfa994ab24cfae064
Nico Bellanich <nicolasbellanich@gmail.com>
2020-06-03 15:11:12 -0300

    change Onclick to switch

--- src/Landing.js
+++ src/Landing.js
@@ -4,24 +4,27 @@ import SingInForm from "./SingIn";
 import SingUpForm from "./SingUp";
 
 function LandingForm() {
-  const [toggle, setToggle] = React.useState(false);
+  const [toggle, setToggle] = React.useState(true);
+  const [toggle2, setToggle2] = React.useState(false)
 
   return (
     <div>
       <Form className="text-center" style={{ width: "100%" }}>
         <Form.Group controlId="principal">
-          <SingInForm></SingInForm>
+         <div>{toggle && <SingInForm></SingInForm>}</div>
+         <div>{toggle2 && <SingUpForm></SingUpForm>}</div>
         </Form.Group>
         <Form.Group>
           <Form.Text
             className="text-muted"
-            onClick={() => setToggle((toggle) => !toggle)}
+            onClick={() => {
+            setToggle((toggle) => !toggle)
+            setToggle2((toggle2) => !toggle2)}}
           >
             Don't you have an account?
           </Form.Text>
         </Form.Group>
       </Form>
-      <div>{toggle && <SingUpForm></SingUpForm>}</div>
     </div>
   );
 }
