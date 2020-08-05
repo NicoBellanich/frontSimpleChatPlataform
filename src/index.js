b1b7f6427ef888dab6aa10325689d987063bbd46
Nico Bellanich <nicolasbellanich@gmail.com>
2020-08-05 14:44:52 -0300

    Exctract apollo Client logic to it's own component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import LandingForm from './Landing';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { ApolloProvider } from 'react-apollo'
+
+import * as serviceWorker from './serviceWorker'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ApolloClient from './ApolloClient'
+
+import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <LandingForm></LandingForm>
-  </React.StrictMode>,
+  <ApolloProvider client={ApolloClient}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
-);
-//  <App />
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
+
+serviceWorker.unregister()
